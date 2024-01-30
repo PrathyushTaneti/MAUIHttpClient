0d99c67599f1ccae142e484b4c963abbf6ccf864
Prathyush Taneti <prathyush@iqdynamics-tech.com>
2024-01-30 13:46:17 +0530

    changed architecture

---
+++ MAUIHttpClient.Client.ARM/Pages/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MAUIHttpClient.Client.ARM.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAPIButtonClicked(object sender, EventArgs e)
+        {
+            //var httpClient = new HttpClient();
+        }
+    }
+
+}
